47c6d4f1d662f7eb4c2bb816ac01b0fed20b74cd
Danny <dannyahnguyen@gmail.com>
2021-10-14 12:02:27 -0400

    import keys from private file

---
+++ respond_to_tweet.py
@@ -0,0 +1,25 @@
+import webbrowser 
+import tweepy
+import consumer_keys
+
+consumer_key = consumer_keys.get_consumer_key()
+consumer_secret = consumer_keys.get_consumer_secret()
+
+callback_uri = 'oob'
+
+# Authenticate to Twitter
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret, callback_uri)
+
+# get user pin and tokens
+redirect_url = auth.get_authorization_url()
+webbrowser.open(redirect_url)
+user_pin_input = input("What is the pin value? ")
+auth.get_access_token(user_pin_input)
+
+# Create API object
+api = tweepy.API(auth)
+
+# Create a tweet
+# api.update_status("Hello Tweepy")
+settings = api.get_settings()
+print(settings['screen_name'])
