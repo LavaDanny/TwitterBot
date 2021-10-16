475c3e0b8001e09391cddea67c42a4d7506f3419
Danny <dannyahnguyen@gmail.com>
2021-10-15 21:26:59 -0400

    output tweets with keyword 'aliens'

--- respond_to_tweet.py
+++ respond_to_tweet.py
@@ -1,14 +1,20 @@
 import webbrowser 
 import tweepy
 import consumer_keys
+import os
+import json
 
 consumer_key = consumer_keys.get_consumer_key()
 consumer_secret = consumer_keys.get_consumer_secret()
+
+access_token =  consumer_keys.get_access_token()
+access_secret = consumer_keys.get_access_secret()
 
 callback_uri = 'oob'
 
 # Authenticate to Twitter
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret, callback_uri)
+auth.set_access_token(access_token, access_secret)
 
 # get user pin and tokens
 redirect_url = auth.get_authorization_url()
@@ -19,7 +25,8 @@ auth.get_access_token(user_pin_input)
 # Create API object
 api = tweepy.API(auth)
 
-# Create a tweet
-# api.update_status("Hello Tweepy")
-settings = api.get_settings()
-print(settings['screen_name'])
+# get tweets mentioning "aliens"
+tweets = api.search_tweets("aliens")
+
+for i in tweets:
+    print(i.text)
